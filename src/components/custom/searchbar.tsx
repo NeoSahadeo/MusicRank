4a94c612d070c042ecfb43cc9630bf9d3688be1c
NeoSahadeo <neosahadeo@protonmail.com>
2025-10-05 14:17:48 +0200

    [fix] Patched non-debounced search results to now be debounce

--- src/components/custom/searchbar.tsx
+++ src/components/custom/searchbar.tsx
@@ -1,39 +1,29 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { useDebounce } from "use-debounce";
-import { resetTracks, addTracks } from "@/stores/tracks";
-import { $search, setSearch } from "@/stores/search";
-import { useStore } from "@nanostores/react";
+import { setSearch } from "@/stores/search";
 
 export default function SearchBar() {
-	const search = useStore($search);
-	const [value] = useDebounce(search, 1000);
+  const [localSearch, setLocalSearch] = useState("");
+  const [value] = useDebounce(localSearch, 1000);
 
-	useMemo(async () => {
-		if (value) {
-			const res = await fetch(
-				`${import.meta.env.BASE_URL}api/proxy?url=https://api.deezer.com/search?q=${value}`,
-			);
-			if (res.ok) {
-				resetTracks();
-				const json = await res.json();
-				console.log(json.data);
-				addTracks(json.data);
-			}
-		} else {
-			// TODO: Load back default state
-		}
-	}, [value]);
+  useMemo(async () => {
+    if (value) {
+      setSearch(localSearch);
+    } else {
+      setSearch("");
+    }
+  }, [value]);
 
-	return (
-		<>
-			<input
-				type="text"
-				placeholder="Search"
-				defaultValue={""}
-				onChange={(e) => {
-					setSearch(e.target.value);
-				}}
-			/>
-		</>
-	);
+  return (
+    <>
+      <input
+        type="text"
+        placeholder="Search"
+        defaultValue={""}
+        onChange={(e) => {
+          setLocalSearch(e.target.value);
+        }}
+      />
+    </>
+  );
 }
